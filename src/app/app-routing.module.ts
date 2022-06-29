78e8b14f3b5abe943faab0f050e12a59d7505ab3
emulcahy <emulcahy@gmail.com>
2022-06-28 23:21:53 -0400

    Shell out a few login components, implement auth service - fix import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
-import {AuthGuard} from "@angular/fire/auth-guard";
+import { AuthGuard } from './shared/guard/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: '/sign-in', pathMatch: 'full' },
